6571bf77204824e529400f1c1d3bb0f49f696622
SergeyVolik <volik.sergey112@gmail.com>
2024-06-24 12:49:09 +0300

    :truck:  move quaternion code to sample

--- build/three.module.js
+++ build/three.module.js
@@ -3403,25 +3403,6 @@ class Quaternion {
 
 	}
 
-	setFromEulerXYZ(x, y, z)
-	{
-		const cos = Math.cos;
-		const sin = Math.sin;
-
-		const c1 = cos( x / 2 );
-		const c2 = cos( y / 2 );
-		const c3 = cos( z / 2 );
-
-		const s1 = sin( x / 2 );
-		const s2 = sin( y / 2 );
-		const s3 = sin( z / 2 );
-		
-		this._x = s1 * c2 * c3 + c1 * s2 * s3;
-		this._y = c1 * s2 * c3 - s1 * c2 * s3;
-		this._z = c1 * c2 * s3 + s1 * s2 * c3;
-		this._w = c1 * c2 * c3 - s1 * s2 * s3;
-	}
-
 	setFromEuler( euler, update = true ) {
 
 		const x = euler._x, y = euler._y, z = euler._z, order = euler._order;
